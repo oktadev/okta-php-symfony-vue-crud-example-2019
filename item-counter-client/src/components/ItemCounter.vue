9171d7f5c8fd497c3997bdfb7a4c016794d8d67b
Krasimir Hristozov <krasi.hristozov@gmail.com>
2019-06-25 02:40:08 +0300

    Frontend app skeleton with Okta authentication

---
+++ item-counter-client/src/components/ItemCounter.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>This is the item counter</h1>
+</template>
+
+<script>
+</script>
--- item-counter-client/src/main.js
+++ item-counter-client/src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+import Dashboard from './components/Dashboard.vue'
+import ItemCounter from './components/ItemCounter.vue'
+import Auth from '@okta/okta-vue'
+
+Vue.use(Auth, {
+  issuer: 'https://dev-354685.oktapreview.com/oauth2/default',
+  client_id: '0oalsm9qsj3xPnVxM0h7',
+  redirect_uri: 'http://localhost:8080/implicit/callback',
+  scope: 'openid profile email'
+})
+
+const routes = [
+  { path: '/implicit/callback', component: Auth.handleCallback() },
+  { path: '/counter', component: ItemCounter }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(Dashboard)
 }).$mount('#app')
